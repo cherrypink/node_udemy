8464618ba2ac20e26f1e441d897cf30e61bef5bd
jprogram <joosangkim1@gmail.com>
2016-12-12 08:12:10 +0900

    blog

---
+++ nodeblog/routes/posts.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/add', function(req, res, next) {
+  res.render('addpost',{
+  	'title' : 'Add Post'
+  });
+});
+
+module.exports = router;
