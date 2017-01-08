fda0b3c40f7b6ce6167a457952bfc8e898eb7e20
jprogram <joosangkim1@gmail.com>
2017-01-09 00:30:29 +0900

    complete portfolio

---
+++ portfolio/public/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	$('.delete-project').on('click',function(){
+		var id =  $(this).data('id');
+		var url = '/admin/delete/'+id;
+
+		if(confirm('Delete Project?')){
+				$.ajax({
+				url: url,
+				type:'DELETE',
+				success : function(result){
+					window.location = '/admin';
+				},
+				error : function(err){
+					alert(err);
+					console.log(err);
+				}
+			});
+
+		}
+	});
+})
